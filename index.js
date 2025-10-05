8e2497eaa78a5ccb0e292199ac31a879c2c1507b
iamakilelias <akilelias9@gmail.com>
2025-10-05 00:51:58 -0400

    back to the drawing board

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ document.getElementById("job-letter-form").addEventListener("submit", async (e) 
     e.preventDefault();
     const formData = Object.fromEntries(new FormData(e.target).entries());
 
-    await fetch("https://script.google.com/macros/s/AKfycby8V4tIW4ttH-h-2g8wXQ5AGd-cB-_E7S6BZNmvUNA7DHoaG8dsF__A0fROOKzUCPp_JQ/exec", {
+    await fetch("", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(formData)
